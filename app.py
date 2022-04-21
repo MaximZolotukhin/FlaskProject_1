30957c22218a3cc90e5eec0c4e0b138e41d92c89
Maxim <ScorpionEvil777@gmail.com>
2022-04-21 23:13:22 +0300

    Создан пустой проект

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
