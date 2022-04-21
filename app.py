91d89a5cd34597a27c46441b0e156d2cdf4270e9
Maxim <ScorpionEvil777@gmail.com>
2022-04-22 00:01:15 +0300

    Добавлено меню

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index/')
 def index():
-    return 'Здесь будет главная'
+    return render_template('index.html')
 
 #Направления
 @app.route('/departures/<departure>/')
