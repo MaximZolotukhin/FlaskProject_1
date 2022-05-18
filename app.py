e285670f8ed1938fad988c208fcb598340653c5d
Maxim <ScorpionEvil777@gmail.com>
2022-05-19 00:29:43 +0300

    Доработал страницу с ниформацией по туру

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import title, subtitle, description, departures, tours
 import os
 
 app = Flask(__name__)
+dep_page = ''
+
+# @app.errorhandler(404)
+# def render_error_page(error):
+#     return "Ничего не нашлось! Вот неудача, отправляйтесь на главную!", error
 
 #Главная страница
 @app.route('/')
 @app.route('/index/')
-def index():
+def render_index():
     tours_index_page = []
     for count, value in tours.items():
         if count < 7:
@@ -15,35 +20,23 @@ def index():
         else:
             break
 
-    return render_template('index.html', title=title, subtitle=subtitle, description=description, departures=departures, tours=tours_index_page)
+    return render_template('index.html', title=title, subtitle=subtitle, description=description, departures=departures, tours=tours)
 
+#Вьюха страницы туры
+@app.route('/departure/<depart>') #<str:depar> Динамически предающаяся часть страницы
+def render_departure(depart): #Параметр для дальнейшей работы в вьюхе
+    tours_index_page = []
+    for count, value in tours.items():
+        tours_index_page.append(value)
+    print(tours_index_page)
 
-@app.route('/departure/')
-def departure():
-    return render_template('spb.html', title=title, subtitle=subtitle, description=description, departures=departures, tours=tours)
-#
-#
-# #Направления
-# @app.route('/departures/<departure>/')
-# def departures(departure):
-#     return render_template('departure.html', title=title)
-#
-# #Туры
-# @app.route('/tours/<id>')
-# def tours(id):
-#     return render_template('tour.html', title=title)
-#
-# @app.route('/data/')
-# def data():
-#     return render_template('data.html')
-#
-# @app.route('/data/departures/<departure>/')
-# def data_departure(departure):
-#     return render_template('data.html', departure=departure)
-#
-# @app.route('/data/tours/<id>/')
-# def data_tours(id):
-#     return render_template('cuba.html')
+    return render_template('departure.html', description=description, departures=departures, tours=tours, departure=depart, tour_doct=tours_index_page)
+    #depar=depar передаю переменну в шаблон для отрисовки только нужных данных
+
+@app.route('/tour/<int:id>') #<str:depar> Динамически предающаяся часть страницы
+def render_tour(id): #Переменная для фильтарции
+    return render_template('tour.html', departures=departures, tours=tours, id=id)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
